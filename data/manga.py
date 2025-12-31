05e8645da39a76c8f42d0daedb20a5f3f35e3981
SpreadSheets600 <sohammaity239@gmail.com>
2025-12-31 12:04:21 +0530

    feat: add fetch_manga function to retrieve user manga list from Anilist API

---
+++ data/manga.py
@@ -0,0 +1,36 @@
+import httpx
+
+MANGA_QUERY = """
+query ($username: String) {
+  MediaListCollection(userName: $username, type: MANGA) {
+    lists {
+      entries {
+        score
+        progress
+        progressVolumes
+        repeat
+        status
+        updatedAt
+        completedAt { year month }
+        media {
+          title { romaji }
+          countryOfOrigin
+          genres
+          bannerImage
+          coverImage { large }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+async def fetch_manga(username: str):
+    async with httpx.AsyncClient() as client:
+        r = await client.post(
+            "https://graphql.anilist.co",
+            json={"query": MANGA_QUERY, "variables": {"username": username}},
+        )
+        r.raise_for_status()
+        return r.json()["data"]["MediaListCollection"]
